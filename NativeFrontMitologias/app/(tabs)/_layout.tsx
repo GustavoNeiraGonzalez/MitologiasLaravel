93c869beba1d5c5c8887111b80ed90803895f0b2
Gustavo <gustavoeduardoneiragonzalez@gmail.com>
2025-10-30 16:08:56 -0300

    hemos creado cierta estilizacion en tabs y ademas agregamos un icono a index que cambia si esta o no presionado, junto con que tenga el mismo color al texto que pusimos anteriormente

--- NativeFrontMitologias/app/(tabs)/_layout.tsx
+++ NativeFrontMitologias/app/(tabs)/_layout.tsx
@@ -1,11 +1,18 @@
 import { Tabs } from "expo-router";
-
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 export default function TabsLayout() {
   return (
     <>
-      <Tabs>
-        <Tabs.Screen name="Login" options={{ title: "Login Page" }} />
-        <Tabs.Screen name="index" options={{ title: "Home Page" }} />
+      <Tabs screenOptions={{tabBarActiveTintColor:"coral", tabBarActiveBackgroundColor:"#225a8fff",tabBarInactiveTintColor:"white", tabBarInactiveBackgroundColor:"#120b1fff"}}>
+        <Tabs.Screen name="login" options={{ title: "Login Page" }} />
+        <Tabs.Screen name="index" options={{ title: "Home Page",
+        tabBarIcon:({color,focused})=>{
+          return focused ? 
+          (<MaterialCommunityIcons name="home-heart" size={24} color={color} />) : 
+          (<FontAwesome name="home" size={24} color={color} />)
+        },
+         }} />
       </Tabs>
     </>
   );
