49808b9edf4bdc3db48ec036d88f6a91ac565762
Gustavo <gustavoeduardoneiragonzalez@gmail.com>
2025-12-06 23:40:22 -0300

    instalado axios, avance en obtener titulo como prueba de que sirva el api

--- NativeFrontMitologias/app/(tabs)/index.tsx
+++ NativeFrontMitologias/app/(tabs)/index.tsx
@@ -1,13 +1,36 @@
 import { StyleSheet,Text, View } from "react-native";
 import {Link} from "expo-router";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+interface Mitologia {
+  id: number;
+  titulo: string;
+}
 export default function Index() {
+  const [mitologias, setMitologias] = useState<Mitologia[]>([]);
+
+    useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/api/mitologias")
+      .then((response) => {
+        // Asumiendo que la API devuelve { Mitologias: [...] }
+        setMitologias(response.data.Mitologias);
+      })
+      .catch((err) => {
+        console.log("Error al obtener mitologías:", err);
+      });
+  }, []);
+
+
   return (
-    <View
-      style={styles.container}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link href="/login">Go to Login page</Link>
+   <View style={styles.container}>
+      <Text>Mitologías disponibles:</Text>
+
+      {mitologias.map((item) => (
+        <Text key={item.id}>📘 {item.titulo}</Text>
+      ))}
+
+      <Link href="/login">Ir a Login</Link>
     </View>
   );
 }
